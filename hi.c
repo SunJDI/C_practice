ddbdf791f238ddef926b17afe39b86b403f9f507
kdaeho <kimdaeho1127@naver.com>
2017-03-31 16:10:36 +0900

    Update hi.c

--- hi.c
+++ hi.c
@@ -2,9 +2,37 @@ https://sourceforge.net/projects/orwelldevcpp/             C 다운
 
 
 // Hello world 출력
+/*
 #include<stdio.h>
 
 int main(void)
 {
   printf("Hello world");
 }
+*/
+
+----------------------------------
+  
+/*
+#include<stdio.h>
+
+int main(void)
+{
+  int a;
+  int b;
+  printf("%d+%d=%d",a,b,a+b);
+}
+*/
+  
+#include<stdio.h>
+  int main(void)
+{
+  int a;
+  int b;
+  printf("  
+         
+         
+         
+         
+         
+         
