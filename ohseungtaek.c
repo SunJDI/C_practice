0666641e3b87647ce9824f3910e1181e3f001f88
oseungtaek <tmdxor2580@naver.com>
2017-04-07 17:22:52 +0900

    Update ohseungtaek.c

--- ohseungtaek.c
+++ ohseungtaek.c
@@ -25,3 +25,17 @@ int main(void)
 	scanf("%d",&b);
 	printf("%d*%d=%d",a,b,a*b);
 }
+
+#include <stdio.h>
+int main(void)
+{
+	int a;
+	int b;
+	printf("수입력");
+	scanf("%d",&a);
+	scanf("%d",&b);
+	printf("%d-%d=%d\n",a,b,a-b);
+	printf("%d+%d=%d\n",a,b,a+b);
+	printf("%d*%d=%d\n",a,b,a*b);
+	printf("%d/%d=%d\n",a,b,a/b);
+}
