c8873eaad4dd1353bea1222e721a7efba20d1be3
sunjdimaster <i9100temp@gmail.com>
2017-05-03 08:36:46 +0900

    Create for문.c

---
+++ for문.c
@@ -0,0 +1,9 @@
+
+#include <stdio.h>	
+void   main( ){
+	int i;                                                    //i 같은 변수를 반복수라 함 (묵시적으로 i부터 씀)
+	for(i = 0; i<4; i++)
+	{
+		printf("i=%d\n",i);
+	}
+}
