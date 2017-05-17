99647dafef89fbabfcbd5d23353c350167b9d39b
dkzpdlemdbtn <dkzpdlemdbtn@naver.com>
2017-05-17 17:49:38 +0900

    Update Yu_Soo's_practice.c

--- Yu_Soo's_practice.c
+++ Yu_Soo's_practice.c
@@ -20,3 +20,7 @@ scanf("%d",&a);  
 scanf("%d",&b);   
 printf("%d-%d=%d",a,b,a-b);
 }
+/*
+#include<stdio.h>
+int main(){ int a; int b; scanf("%d",&b); scanf("%d",&a); printf("망고 븅신"); if((a+b)==3){  printf("망고 븅신");  while((a+b)==3){   printf("하 내가 왜 이러고 사나");  } }  }
+*/
